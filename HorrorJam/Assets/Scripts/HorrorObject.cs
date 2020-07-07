7650054667e8bbdbb47ed7bafc066ffe6c965b1a
paul61302 <paul61302@gmail.com>
2020-07-07 16:20:43 +0800

    123

---
+++ HorrorJam/Assets/Scripts/HorrorObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HorrorObject : MonoBehaviour, ITriggerEvent
+{
+    public void TriggerEvent()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") TriggerEvent();
+    }
+}
